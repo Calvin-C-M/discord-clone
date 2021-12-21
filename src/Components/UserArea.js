abcdc0b191ae62f1a2f8b087d5399906eb220212
Calvin-C-M <montolalu28@gmail.com>
2021-12-21 21:34:49 +0700

    Added User Area in friends menu

--- src/Components/UserArea.js
+++ src/Components/UserArea.js
@@ -1,10 +1,14 @@
 import "../assets/styles/UserArea.scss"
 import { useState } from "react";
-import { MdMic, MdMicOff } from "react-icons/md"
+import { MdMic } from "react-icons/md"
 import { RiHeadphoneFill } from "react-icons/ri"
-import { BsGearFill } from "react-icons/bs"
+import { BsGearFill, BsEmojiLaughingFill } from "react-icons/bs"
 import ProfileDisplay from "./ProfileDisplay";
 import photo from "../assets/img/discord-logo.png"
+import OnlineStatus from "../Statuses/Online";
+import IdleStatus from "../Statuses/Idle";
+import BusyStatus from "../Statuses/Busy";
+import OfflineStatus from "../Statuses/Offline";
 
 const UserArea = () => {
     const user = {
@@ -24,19 +28,71 @@ const UserArea = () => {
 
     const [micOn, setMicOn] = useState(false);
     const [deafenOn, setDeafenOn]=useState(false);
+    const [showChangeStatus, setShowChangeStatus]=useState(false);
     const [showLabelMic, setShowLabelMic]=useState(false);
     const [showLabelDeafen, setShowLabelDeafen]=useState(false);
     const [showLabelSetting, setShowLabelSetting]=useState(false);
 
+    const changeMicStatus = () => (micOn) ? setMicOn(false) : setMicOn(true)
+    const changeDeafenStatus = () => (deafenOn) ? setDeafenOn(false) : setDeafenOn(true)
+    const changeChangeStatus = () => (showChangeStatus) ? setShowChangeStatus(false) : setShowChangeStatus(true)
+
     return (
         <section className="user-area">
             <section className="profile-detail">
-                <ProfileDisplay 
-                    photo={user.profile.src}
-                    status={user.status}
-                    width={user.profile.width}
-                    height={user.profile.height}
-                />
+                {
+                    showChangeStatus
+                    &&
+                    <section className="change-status">
+                        <section id="online">
+                            <section className="status">
+                                <OnlineStatus />
+                                <span>
+                                    Online
+                                </span>
+                            </section>
+                        </section>
+                        <section id="others">
+                            <section className="status">
+                                <IdleStatus />
+                                <span>
+                                    Idle
+                                </span>
+                            </section>
+                            <section className="status">
+                                <BusyStatus />
+                                <span>
+                                    Do Not Disturb
+                                </span>
+                            </section>
+                            <section className="status">
+                                <OfflineStatus />
+                                <span>
+                                    Invisible
+                                </span>
+                            </section>
+                        </section>
+                        <section id="custom">
+                            <section className="status">
+                                <BsEmojiLaughingFill />
+                                <span>
+                                    Set a Custom Status
+                                </span>
+                            </section>
+                        </section>
+                    </section>
+                }
+                <section 
+                    className="display"
+                    onClick={changeChangeStatus}
+                >
+                    <ProfileDisplay 
+                        photo={user.profile.src}
+                        status={user.status}
+                        width={user.profile.width}
+                        height={user.profile.height}
+                    />
+                </section>
                 <section className="user-detail">
                     <p>{user.name}</p>
                     <span>#{user.id}</span>
@@ -48,39 +104,54 @@ const UserArea = () => {
                     onMouseEnter={() => setShowLabelMic(true)}
                     onMouseLeave={() => setShowLabelMic(false)}
                 >
-                    { showLabelMic && 
+                    { 
+                        showLabelMic 
+                        && 
                         <span>
-                            Unmute
+                            { (micOn) ? "Unmute" : "Mute" }
                             <div className="triangle"></div>
                         </span>
                     }
-                    <MdMic size={icon.size} />
+                    <MdMic onClick={changeMicStatus} size={icon.size} />
+                    {
+                        micOn
+                        &&
+                        <div className="line"></div>
+                    }
                 </section>
                 <section
                     className="icons"
                     onMouseEnter={() => setShowLabelDeafen(true)}
                     onMouseLeave={() => setShowLabelDeafen(false)}
                 >
-                    { showLabelDeafen && 
+                    { 
+                        showLabelDeafen 
+                        && 
                         <span>
-                            Deafen
+                            { (deafenOn) ? "Undeafen" : "Deafen" }
                             <div className="triangle"></div>
                         </span>
                     }
-                    <RiHeadphoneFill size={icon.size} />
+                    <RiHeadphoneFill onClick={changeDeafenStatus} size={icon.size} />
+                    {
+                        deafenOn
+                        &&
+                        <div className="line"></div>
+                    }
                 </section>
                 <section 
                     className="icons"
                     onMouseEnter={() => setShowLabelSetting(true)}
                     onMouseLeave={() => setShowLabelSetting(false)}
                 >
-                    { showLabelSetting && 
+                    { 
+                        showLabelSetting 
+                        && 
                         <span id="user-setting">
                             User Settings
                             <div className="triangle"></div>
                         </span>
                     }
-
                     <BsGearFill size={icon.size} />
                 </section>
             </section>
